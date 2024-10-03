3d1f4847b766a1d3035c91017789314379ed70ee
Joey Knowles <Joey.Knowles@stu.crc-lennox.qc.ca>
2024-10-03 15:50:04 -0400

    Initial commit for the singleton feature.

--- src/main/java/com/champlain/oop2assignment3/Deck.java
+++ src/main/java/com/champlain/oop2assignment3/Deck.java
@@ -18,16 +18,25 @@ public class Deck extends CardCollection implements CardSource {
      */
     private final List<Card> aCards = new ArrayList<>();
 
+    private static Deck instance;
+
     /**
      * Constructs a new Deck containing all standard playing cards.
      * The deck is initialized with one of each rank and suit combination.
      */
-    public Deck() {
+    private Deck() {
         for (Rank currentRank : Rank.values()) {
             for (Suit currentSuit : Suit.values()) {
                 this.aCards.add(new Card(currentRank, currentSuit));
             }
         }
+    }
+
+    public static Deck getInstance() {
+        if (instance == null) {
+            instance = new Deck();
+        }
+        return instance;
     }
 
     /**
